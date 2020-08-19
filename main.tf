89a1275a58d878f576b25e2939e3fc0ae475b342
Ubuntu <ubuntu@ip-172-31-46-121.eu-west-1.compute.internal>
2020-08-19 16:23:28 +0000

    Fixed ssh issue

--- main.tf
+++ main.tf
@@ -14,15 +14,61 @@ resource "aws_vpc" "vpc" {
 
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_subnet" "subnet_a" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.sn_cidr_block
   availability_zone = data.aws_availability_zones.available.names[0]
 }
 
-resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = var.instance
-  key_name      = var.key
+
+resource "aws_internet_gateway" "TerraformIG" {
+  vpc_id = aws_vpc.vpc.id
 }
 
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = var.ext_cidr_block
+    gateway_id = aws_internet_gateway.TerraformIG.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet_a.id
+  route_table_id = aws_route_table.r.id
+}
+
+resource "aws_security_group" "allowssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                         = var.ami
+  instance_type               = var.instance
+  key_name                    = var.key
+  subnet_id                   = aws_subnet.subnet_a.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = ["${aws_security_group.allowssh.id}"]
+}
--- variables.tf
+++ variables.tf
@@ -30,3 +30,7 @@ variable "cidr_block" {
 variable "sn_cidr_block" {
   default = "172.16.0.0/24"
 }
+
+variable "ext_cidr_block" {
+  default = "0.0.0.0/0"
+}
