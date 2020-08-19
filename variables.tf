a5010e410c539aec150d0ab4f5301d76fc1531fb
Ubuntu <ubuntu@ip-172-31-46-121.eu-west-1.compute.internal>
2020-08-19 13:11:04 +0000

    Fixed Format

--- variables.tf
+++ variables.tf
@@ -24,9 +24,9 @@ variable "shared_credentials" {
 }
 
 variable "cidr_block" {
-  default     = "172.16.0.0/16"
+  default = "172.16.0.0/16"
 }
 
 variable "sn_cidr_block" {
-  default     = "172.16.0.0/24"
+  default = "172.16.0.0/24"
 }
