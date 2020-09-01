34c540511674e4f7e4ac04bf13a8306c2534b797
sipeki <sipeki@kindlen.eu>
2020-09-01 11:06:18 +0000

    edit of initDB.sql commit out line 7. As syntax error.

--- setupDB/initDatabase.sh
+++ setupDB/initDatabase.sh
@@ -4,4 +4,4 @@ echo "initialize Database"
 mysql -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_PASSWORD < initDB.sql
 
 echo "Populate Database"
-mysql -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_PASSWORD < populateDB.sql
+mysql -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_PASSWORD petclinic < populateDB.sql
