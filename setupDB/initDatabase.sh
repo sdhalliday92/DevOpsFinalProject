48b767ac48d03a4439f24b07242a5ec040716a4f
sipeki <sipeki@kindlen.eu>
2020-09-01 10:23:20 +0000

    sheel script fro running database setup

---
+++ setupDB/initDatabase.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "initialize Database"
+mysql -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_PASSWORD < initDB.sql
+
+echo "Populate Database"
+mysql -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_PASSWORD < populateDB.sql
